ff2e68102e130ca8778849b00b7609d17a098d3a
rasel-33 <87210321+rasel-33@users.noreply.github.com>
2022-03-13 16:14:25 +0600

    Update MainActivity.java

--- app/src/main/java/com/rasel/sudokusolver/MainActivity.java
+++ app/src/main/java/com/rasel/sudokusolver/MainActivity.java
@@ -28,47 +28,47 @@ public class MainActivity extends AppCompatActivity {
 
     public void BTNOnePress(View view){
         gameBoardSolver.setNumberPos(1);
-        gameBoard.invalidate(this);
+        gameBoard.invalidate();
     }
 
     public void BTNTwoPress(View view){
         gameBoardSolver.setNumberPos(2);
-        gameBoard.invalidate(this);
+        gameBoard.invalidate();
     }
 
     public void BTNThreePress(View view){
         gameBoardSolver.setNumberPos(3);
-        gameBoard.invalidate(this);
+        gameBoard.invalidate();
     }
 
     public void BTNFourPress(View view){
         gameBoardSolver.setNumberPos(4);
-        gameBoard.invalidate(this);
+        gameBoard.invalidate();
     }
 
     public void BTNFivePress(View view){
         gameBoardSolver.setNumberPos(5);
-        gameBoard.invalidate(this);
+        gameBoard.invalidate();
     }
 
     public void BTNSixPress(View view){
         gameBoardSolver.setNumberPos(6);
-        gameBoard.invalidate(this);
+        gameBoard.invalidate();
     }
 
     public void BTNSevenPress(View view){
         gameBoardSolver.setNumberPos(7);
-        gameBoard.invalidate(this);
+        gameBoard.invalidate();
     }
 
     public void BTNEightPress(View view){
         gameBoardSolver.setNumberPos(8);
-        gameBoard.invalidate(this);
+        gameBoard.invalidate();
     }
 
     public void BTNNinePress(View view){
         gameBoardSolver.setNumberPos(9);
-        gameBoard.invalidate(this);
+        gameBoard.invalidate();
     }
 
     public void solve(View view){
